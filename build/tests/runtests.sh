b9a51f48035691290df8a8fd4e6144c42f5e0a4e
Dominic Watson <watson.dominic@gmail.com>
2018-07-10 18:28:16 +0100

    Attempt to make test run work on travis

--- build/tests/runtests.sh
+++ build/tests/runtests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd `dirname $0`
+CWD="`pwd`"
 
 box install || exit 1;
 
@@ -13,9 +14,9 @@ declare -a engines=("lucee@4.5.5" "adobe@10" "adobe@11", "adobe@2016" )
 ## now loop through the above array
 for engine in "${engines[@]}"
 do
-	echo "Running tests on engine: $engine";
+	echo "Running tests on engine: $engine, using directory: $CWD and server config: $CWD/server-cfconcurrenttests.json";
 
-	box start directory="./" serverConfigFile="./server-cfconcurrenttests.json" cfengine="$engine" save=false;
+	box start directory="$CWD" serverConfigFile="$CWD/server-cfconcurrenttests.json" cfengine="$engine" save=false;
 	box testbox run verbose=false || exitcode=1
 	box stop name="cfconcurrenttests"
 done
