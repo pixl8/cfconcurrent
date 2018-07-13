19e251d3f9cc4289fee68963066c9f2d83a037eb
Dominic Watson <watson.dominic@gmail.com>
2018-07-13 09:27:53 +0100

    Add ACF2018 to the test suite :)

--- build/tests/runtests.sh
+++ build/tests/runtests.sh
@@ -9,16 +9,19 @@ exitcode=0
 
 box stop name="cfconcurrenttests"
 
-declare -a engines=("lucee@4.5.5" "adobe@11" "adobe@2016" )
+declare -a engines=("lucee@4.5.5" "adobe@11" "adobe@2016" "adobe@2018" )
 
 ## now loop through the above array
 for engine in "${engines[@]}"
 do
-	echo "Running tests on engine: $engine, using directory: $CWD and server config: $CWD/server-cfconcurrenttests.json";
+	echo "Running tests on engine: $engine";
 
 	box start directory="$CWD" serverConfigFile="$CWD/server-cfconcurrenttests.json" cfengine="$engine" saveSettings=false;
 	box testbox run verbose=false || exitcode=1
 	box stop name="cfconcurrenttests"
+
+	echo "sleeping for 5s to allow commandbox to properly shutdown server :)"
+	sleep 5s
 done
 
 exit $exitcode
