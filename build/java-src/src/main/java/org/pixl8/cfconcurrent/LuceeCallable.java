5faa3c1b16cc4b2b5fb21d9d107e12d594a246ad
Dominic Watson <watson.dominic@gmail.com>
2019-10-07 00:37:17 +0100

    Refactor to reduce duplication and simplify calling slightly

--- build/java-src/src/main/java/org/pixl8/cfconcurrent/LuceeCallable.java
+++ build/java-src/src/main/java/org/pixl8/cfconcurrent/LuceeCallable.java
@@ -1,71 +1,23 @@
 package org.pixl8.cfconcurrent;
 
-import java.io.File;
-
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-
-import lucee.loader.engine.*;
 import lucee.runtime.exp.PageException;
 import lucee.runtime.Component;
-import lucee.runtime.PageContext;
 import lucee.runtime.listener.ApplicationContext;
-import java.util.HashMap;
 import java.util.concurrent.Callable;
 
 public class LuceeCallable implements Callable {
-	private CFMLEngine         lucee;
-	private Component          callableCfc;
-	private File               contextRoot;
-	private Long               timeout;
-	private ApplicationContext appContext;
+	private LuceeCfcProxy proxy;
 
 // CONSTRUCTOR
-	public LuceeCallable(
-		  Component          callableCfc
-		, String             contextRoot
-		, ApplicationContext appContext
-		, Long               timeout
-	) throws PageException, ServletException {
-		this.lucee       = CFMLEngineFactory.getInstance();
-		this.callableCfc = callableCfc;
-		this.contextRoot = new File( contextRoot );
-		this.timeout     = timeout;
-		this.appContext  = appContext;
+	public LuceeCallable( Component callableCfc, String contextRoot, ApplicationContext appContext ) throws PageException, ServletException {
+		this.proxy = new LuceeCfcProxy( callableCfc, contextRoot, appContext );
 	}
 
 // THE RUNNABLE BIT
 	@Override
 	public Object call() throws Exception {
-		return callableCfc.call( _getPageContext(), "call", new Object[0] );
+		return proxy.callMethod( "call" );
 	}
 
-// PRIVATE HELPERS
-	private PageContext _getPageContext() throws ServletException {
-		PageContext pc = lucee.getThreadPageContext();
-
-		if ( pc != null ) {
-			return pc;
-		}
-
-		javax.servlet.http.Cookie[] cookies = new Cookie[]{};
-
-		pc = lucee.createPageContext(
-			  contextRoot
-			, "localhost"    // host
-			, "/"            // script name
-			, ""             // query string
-			, cookies		 // cookies
-			, null           // headers
-			, new HashMap()  // parameters
-			, new HashMap()  // attributes
-			, System.out     // response stream where the output is written to
-			, timeout        // timeout for the simulated request in milli seconds
-			, true           // register the pc to the thread
-		);
-
-		pc.setApplicationContext( appContext );
-
-		return pc;
-	}
 }
--- build/java-src/src/main/java/org/pixl8/cfconcurrent/LuceeRunnable.java
+++ build/java-src/src/main/java/org/pixl8/cfconcurrent/LuceeRunnable.java
@@ -1,76 +1,27 @@
 package org.pixl8.cfconcurrent;
 
-import java.io.File;
-
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-
-import lucee.loader.engine.*;
 import lucee.runtime.exp.PageException;
 import lucee.runtime.Component;
-import lucee.runtime.PageContext;
 import lucee.runtime.listener.ApplicationContext;
-import java.util.HashMap;
 
 public class LuceeRunnable implements Runnable {
-	private CFMLEngine         lucee;
-	private Component          runnableCfc;
-	private File               contextRoot;
-	private Long               timeout;
-	private ApplicationContext appContext;
+	private LuceeCfcProxy proxy;
 
 // CONSTRUCTOR
-	public LuceeRunnable(
-		  Component          runnableCfc
-		, String             contextRoot
-		, ApplicationContext appContext
-		, Long               timeout
-	) throws PageException, ServletException {
-		this.lucee       = CFMLEngineFactory.getInstance();
-		this.runnableCfc = runnableCfc;
-		this.contextRoot = new File( contextRoot );
-		this.timeout     = timeout;
-		this.appContext  = appContext;
+	public LuceeRunnable( Component runnableCfc, String contextRoot, ApplicationContext appContext ) throws PageException, ServletException {
+		this.proxy = new LuceeCfcProxy( runnableCfc, contextRoot, appContext );
 	}
 
 // THE RUNNABLE BIT
 	@Override
 	public void run() {
 		try {
-			runnableCfc.call( _getPageContext(), "run", new Object[0] );
+			proxy.callMethod( "run" );
 		} catch( PageException e ) {
 			e.printStackTrace();
 		} catch( ServletException e ) {
 			e.printStackTrace();
 		}
-	}
-
-// PRIVATE HELPERS
-	private PageContext _getPageContext() throws ServletException {
-		PageContext pc = lucee.getThreadPageContext();
-
-		if ( pc != null ) {
-			return pc;
-		}
-
-		javax.servlet.http.Cookie[] cookies = new Cookie[]{};
-
-		pc = lucee.createPageContext(
-			  contextRoot
-			, "localhost"    // host
-			, "/"            // script name
-			, ""             // query string
-			, cookies		 // cookies
-			, null           // headers
-			, new HashMap()  // parameters
-			, new HashMap()  // attributes
-			, System.out     // response stream where the output is written to
-			, timeout        // timeout for the simulated request in milli seconds
-			, true           // register the pc to the thread
-		);
-
-		pc.setApplicationContext( appContext );
-
-		return pc;
 	}
 }
